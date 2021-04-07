898653a5d0d863dae3304061895f6d39af5dd07b
dmeloni <d.meloni5@campus.unimib.it>
2021-04-07 22:57:28 +0100

    remove old files

--- models/base_estimator.py
+++
@@ -1,7 +0,0 @@
-class BaseEstimator:  
-    def __init__(self, **args):
-        pass
-    def fit(self, X, y, **kwargs):
-        raise NotImplementedError
-    def predict(self, X):
-        raise NotImplementedError
